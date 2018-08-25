bab089b0239a3076eac338f7e2c6070d3e0b560a
JZ6 <yjzuo6@gmail.com>
2018-08-25 17:44:08 -0400

    Added build timetable

--- app/backend/dataInput.js
+++ app/backend/dataInput.js
@@ -2,12 +2,29 @@
 
 import example from './example';
 
-import PriorityQueue from './PriorityQueue'
+import PriorityQueue from './PriorityQueue';
+import axios from 'axios';
 
-const requiredCourses = ['CSC108', 'CSC148', 'CSC165', 'MAT137', 'MAT157', 'MAT135', 'MAT136']
-const req = 'https://cobalt.qas.im/api/1.0/courses/filter?key=RyvDnYdKxsblregFbObVQPtVdlN2CpQW&limit=100&q=code%3A%22CSC%22%20AND%20department%3A%22computer%20science%22%20AND%20term%3A%222018%20Fall%22%20OR%20term%3A%222019%20Winter%22%20'
+const requiredCourses = [
+  'CSC108',
+  'CSC148',
+  'CSC165',
+  'MAT137',
+  'MAT157',
+  'MAT135',
+  'MAT136',
+];
+const req1 =
+  'https://cobalt.qas.im/api/1.0/courses/filter?key=RyvDnYdKxsblregFbObVQPtVdlN2CpQW&limit=100&q=code%3A%22CSC%22%20AND%20department%3A%22computer%20science%22%20AND%20term%3A%222018%20Fall%22%20OR%20term%3A%222019%20Winter%22%20';
 
 export function fetchAllCourses() {
+  const req = 'https://cobalt.qas.im/api/1.0/courses/filter?q=';
+  const key = 'RyvDnYdKxsblregFbObVQPtVdlN2CpQW';
+
+  axios.get(`${req}${key}`).then(response => {
+    console.log(response.data);
+  });
+
   // // url (required), options (optional)
   // fetch('https://cobalt.qas.im/api/1.0/courses/filter?q=code%3A%22CSC%22&key=RyvDnYdKxsblregFbObVQPtVdlN2CpQW&limit=100', {
   //   method: 'get'
@@ -17,18 +34,26 @@ export function fetchAllCourses() {
   //   // Error :(
   // });
 
-  let filteredCourses = filterCourses('UTSG', 1, 2019, "Computer Science", ['Mathematical and Computational Sciences', 'Dept. of Computer & Mathematical Sci (UTSC)']);
+  const filteredCourses = filterCourses('UTSG', 1, 2019, 'Computer Science', [
+    'Mathematical and Computational Sciences',
+    'Dept. of Computer & Mathematical Sci (UTSC)',
+  ]);
 
   // 0 = all, 1 = early, 2 = late.
   buildTimeTable(filteredCourses, 0);
 }
 
-function filterCourses(selectedCampus, yearOfStudy, schoolYear, selectedDepartment, interests) {
+function filterCourses(
+  selectedCampus,
+  yearOfStudy,
+  schoolYear,
+  selectedDepartment,
+  interests,
+) {
   // console.log(example[0]);
-  let filteredCourses = new PriorityQueue();
+  const filteredCourses = new PriorityQueue();
 
-  for (let i = 0; i < example.length; i++) {
-
+  for (let i = 0; i < example.length; i += 1) {
     const {
       id,
       code,
@@ -42,18 +67,20 @@ function filterCourses(selectedCampus, yearOfStudy, schoolYear, selectedDepartme
       campus,
       term,
       meeting_sections,
-      breadths
+      breadths,
+      ,
     } = example[i];
 
     console.log(term);
 
     interests.push(selectedDepartment);
 
-    if (selectedCampus == campus &&
+    if (
+      selectedCampus == campus &&
       yearOfStudy * 100 == level &&
       requiredCourses.includes(code.slice(0, 6)) &&
-      interests.includes(department)) {
-
+      interests.includes(department)
+    ) {
       filteredCourses.enqueue(example[i], meeting_sections.length);
     }
   }
@@ -79,19 +106,18 @@ function buildTimeTable(courseList, timePreference) {
       campus,
       term,
       meeting_sections,
-      breadths
+      breadths,
+      ,
     } = courseList.dequeue().element;
 
     const nextCourseCode = code.slice(0, 6);
     console.log(nextCourseCode);
 
     if (!disAllowedCourses.includes(nextCourseCode)) {
-
       disAllowedCourses.push(nextCourseCode);
 
       const courseExclusions = exclusions.split(',');
       for (let i = 0; i < courseExclusions.length; i++) {
-
         console.log(courseExclusions[i].replace(/[( )]/g, ''));
       }
     }
